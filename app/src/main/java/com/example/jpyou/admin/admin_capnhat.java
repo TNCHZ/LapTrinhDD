ef15410638fe2b7fb1af381bafa433bd243ab957
TNCHZ <dochuong2222@gmail.com>
2024-11-09 22:47:32 +0700

    Chuong 9/11

---
+++ app/src/main/java/com/example/jpyou/admin/admin_capnhat.java
@@ -0,0 +1,26 @@
+package com.example.jpyou.admin;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.jpyou.R;
+
+public class admin_capnhat extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.admin_capnhat);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
